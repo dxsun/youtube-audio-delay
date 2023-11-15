cb2c7a8bc98bb357d75bfa6d3698c070a9fc33d6
Daniel <dxsun1998@gmail.com>
2023-11-14 19:00:07 -0800

    Fix delay to work only on active tab

--- scripts/content.js
+++ scripts/content.js
@@ -1,111 +1,3 @@
-// const article = document.querySelector("article");
-
-// // `document.querySelector` may return null if the selector doesn't match anything.
-// if (article) {
-//   const text = article.textContent;
-//   const wordMatchRegExp = /[^\s]+/g; // Regular expression
-//   const words = text.matchAll(wordMatchRegExp);
-//   // matchAll returns an iterator, convert to array to get word count
-//   const wordCount = [...words].length;
-//   const readingTime = Math.round(wordCount / 200);
-//   const badge = document.createElement("p");
-//   // Use the same styling as the publish information in an article's header
-//   badge.classList.add("color-secondary-text", "type--caption");
-//   badge.textContent = `⏱️ ${readingTime} min read`;
-
-//   // Support for API reference docs
-//   const heading = article.querySelector("h1");
-//   // Support for article docs with date
-//   const date = article.querySelector("time")?.parentNode;
-
-//   (date ?? heading).insertAdjacentElement("afterend", badge);
-// }
-
-// var context;
-// var filter;
-
-// const init = () => {
-//   if (context === undefined) {
-//     context = new AudioContext();
-//   }
-
-//   if (filter === undefined) {
-//     filter = new DelayNode(context, { maxDelayTime: 5 })
-//     filter.connect(context.destination);
-//   }
-// };
-
-// const attach = () => {
-//   init();
-
-//   const audioElem = document.getElementsByTagName("audio");
-//   const videoElem = document.getElementsByTagName("video");
-
-//   for (const element of [...audioElem, ...videoElem]) {
-//     try {
-//       var mediaSource = context.createMediaElementSource(element)
-//       context.resume();
-//       mediaSource.connect(filter);
-//       console.log("delay attached.");
-//     } catch (e) {
-//       console.log(e)
-//     }
-//   }
-// };
-
-// const setDelay = (delay) => {
-//   delay = delay/1000
-//   if (filter.delayTime.value !== delay)
-//     filter.delayTime.value = delay;
-// }
-
-// window.onload = () => {
-//   attach();
-//   // chrome.storage.sync.get("delay", (value) =>
-//   //   setDelay(value.delay)
-//   // )
-// };
-
-// chrome.runtime.onMessage.addListener(
-//   (request, sender, sendResponse) => {
-//     if (request.delay) {
-//       setDelay(request.delay);
-//       sendResponse({ack: true});
-//     }
-//   }
-// );
-
-// audioDelay = 3
-// const player = document.querySelector('.html5-main-video');
-// const context = new AudioContext();
-// const source = context.createMediaElementSource(player);
-// const delayNode = context.createDelay(audioDelay);
-// delayNode.delayTime.value = 3
-// source.connect(delayNode);
-// delayNode.connect(context.destination);
-
-
-
-// const attach = () => {
-//   init();
-
-//   const audioElem = document.getElementsByTagName("audio");
-//   const videoElem = document.getElementsByTagName("video");
-
-//   for (const element of [...audioElem, ...videoElem]) {
-//     try {
-//       var mediaSource = context.createMediaElementSource(element)
-//       context.resume();
-//       mediaSource.connect(filter);
-//       console.log("delay attached.");
-//     } catch (e) {
-//       console.log(e)
-//     }
-//   }
-// };
-
-console.log("CHECKPOINT1")
-
 const MAX_TIME_DELAY = 10
 
 var delayValue = 0;
@@ -141,27 +33,13 @@ window.onload = () => {
 	delayNode.connect(context.destination);
 };
 
-chrome.storage.onChanged.addListener((changes, area) => {
-	console.log("CHECKPOINT2:  STORAGE CALLBACK ENTERED")
-	console.log(changes)
-	console.log(area)
-	let newDelay = changes.delay?.newValue
-  if (area === 'sync' && changes.delay?.newValue) {
-    // const debugMode = Boolean(changes.options.newValue.debug);
-    // console.log('enable debug mode?', debugMode);
-    // setDebugMode(debugMode);
-    console.log("CHECKPOINT2: LISTENED TO SYNC")
-    setDelay(newDelay)
+chrome.runtime.onMessage.addListener(
+  (request, sender, sendResponse) => {
+  	console.log("LISTENED")
+    if (request.delay) {
+      setDelay(request.delay);
+      sendResponse({success: true});
+    }
+    return true;
   }
-});
-
-// chrome.runtime.onMessage.addListener(
-//   (request, sender, sendResponse) => {
-//   	console.log("LISTENED")
-//     if (request.delay) {
-//       setDelay(request.delay);
-//       sendResponse({ack: true});
-//     }
-//     return true;
-//   }
-// );
+);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,3 @@
-console.log("CHECKPOINT0")
-
-// alert('hello')
-
 const slider = document.getElementById("delay-slider");
 const text = document.getElementById("delay-text");
 
@@ -9,37 +5,25 @@ const text = document.getElementById("delay-text");
 text.onchange = setStorage
 slider.onchange = setStorage
 
-// chrome.storage.sync.get("delay", (value) => {
-//   text.value = value?.delay
-// })
-
-
 // // update display
 slider.oninput = function() {
-  console.log("CHECKPOINTA")
   text.value = this.value;
 }
 text.oninput = function() {
-  console.log("CHECKPOINTB")
   slider.value = this.value;
 }
 
 function setStorage() {
-  let x = this.value
-  chrome.storage.sync.set({ delay: x})
+  let delayValue = this.value
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let activeTab = tabs[0].id
+    console.log("active tab", activeTab)
+    chrome.tabs.sendMessage(activeTab, { delay: delayValue }, function(response) {
+      console.log("got a response")
+      console.log(response)
+    })
+  });
+
   console.log("VALUE1:")
   console.log(this.value)
-
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //   console.log(tabs[0].id)
-  //   chrome.tabs.sendMessage(tabs[0].id, { x }, function(response) {
-  //     console.log("ACKKNOWLEDGE")
-  //     // console.log(response.ack);
-  //   });
-  // });
 }
-
-// chrome.storage.sync.get("delay", (value) => {
-//   text.value = value.delay
-//   slider.value = value.delay
-// })
